f9233a492b8f84ecab66efd863246b8985fc232a
Ruchit2642 <101005880+Ruchit2642@users.noreply.github.com>
2023-04-11 21:32:58 +0530

    documentations

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,22 +1,29 @@
+
+// This component is for the creating line charts
+
+
+
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 
-import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy'; //importing data from dummy.tsx
 import { useStateContext } from '../../contexts/ContextProvider';
 
 const LineChart = () => {
   const { currentMode } = useStateContext();
+  const primaryXAxis = {...LinePrimaryXAxis};
+  const primaryYAxis = {...LinePrimaryYAxis};
 
   return (
     <ChartComponent
       id="line-chart"
-      height="420px"
-      primaryXAxis={LinePrimaryXAxis}
-      primaryYAxis={LinePrimaryYAxis}
-      chartArea={{ border: { width: 0 } }}
-      tooltip={{ enable: true }}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-      legendSettings={{ background: 'white' }}
+      height="420px" //each line is having a height of 420 pixels
+      primaryXAxis ={primaryXAxis } //  x-axis 
+      primaryYAxis={primaryYAxis}   // y-axis
+      chartArea={{ border: { width: 0 } }} 
+      tooltip={{ enable: true }} //enabling user to learn about the icon by hovering the mouse over them
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'} //Current theme is dark with grayish blue background
+      legendSettings={{ background: 'white' }} //If the theme is light-->white background
     >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
@@ -28,3 +35,9 @@ const LineChart = () => {
 };
 
 export default LineChart;
+
+
+
+
+
+
